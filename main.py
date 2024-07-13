bfc40a19f9f11767b965adc7507a46dfb2ef85ee
Peter Kohout <peter.kohout@knapp.com>
2024-07-13 19:41:52 +0200

    now its possible to configure k8s config via environment.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+import os
 import threading
 import time
 
@@ -5,9 +7,21 @@ from flask import Flask, request, jsonify, render_template
 import subprocess
 
 app = Flask(__name__)
-kube_config_file = "KUBECONFIG=/home/peter/.config/OpenLens/kubeconfigs/755af755-1473-49b0-8ba0-f9645e34f261"
+kube_config_file = "KUBECONFIG=" + os.getenv("K8S_CONFIG")
 namespace_and_container_selection = "--namespace rcll --container refbox"
-
+log = logging.getLogger("controller")
+log.setLevel(logging.INFO)
+# Create a console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+# Create a formatter and set it for the handler
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+# Add the handler to the logger
+log.addHandler(console_handler)
+# Log a message
+log.info("Started logger!")
+log.info(f"Using k8s config file: {kube_config_file}")
 @app.route('/')
 @app.route('/index')
 @app.route('/index.html')
@@ -66,12 +80,16 @@ def stop_all_pods():
     bash_line = kube_config_file + " kubectl delete pods --all --namespace rcll"
     pod_name_cmd = ["/bin/bash", "-c", bash_line]
     process = subprocess.run(pod_name_cmd, capture_output=True, text=True)
+    if process.returncode != 0:
+        raise Exception("Error stopping all pods!")
 
 
 def get_refbox_pod_name():
     bash_line = kube_config_file + " kubectl get pods --namespace rcll -l app=refbox -o custom-columns=\":metadata.name\" | grep refbox"
     pod_name_cmd = ["/bin/bash", "-c", bash_line]
     process = subprocess.run(pod_name_cmd, capture_output=True, text=True)
+    if process.returncode != 0:
+        raise Exception("Error get_refbox_pod_name!")
     pod_name = str(process.stdout).strip()
     return pod_name
 
@@ -79,6 +97,8 @@ def get_points_last_game(kube_config_file, namespace_and_container_selection):
     commandPoints = ["/bin/bash", "-c",
                      kube_config_file + " kubectl logs " + get_refbox_pod_name() + " " + namespace_and_container_selection + "  | grep \"TOTAL POINTS\""]
     resultPoints = subprocess.run(commandPoints, capture_output=True, text=True)
+    if resultPoints.returncode != 0:
+        raise Exception("Error get_points_last_game!")
     return resultPoints.stdout
 
 def get_run_in_refbox_command(command):
@@ -87,23 +107,26 @@ def get_run_in_refbox_command(command):
 
 def start_new_games(amount):
     for i in range(0, amount):
-        print(f"Starting game {i+1}")
+        log.info(f"Starting game {i+1}")
         stop_all_pods()
+        log.info("Waiting 60s buffer time")
         time.sleep(60)
         start_new_game()
+        log.info("Waiting 20min game time")
         time.sleep(20 * 60) #game time
-        time.sleep(120) #buffer time
-        print(f"Last game had: {get_points_last_game(kube_config_file, namespace_and_container_selection)} points!")
+        log.info("Waiting 60s post game buffer time")
+        time.sleep(60) #buffer time
+        log.info(f"Last game had: {get_points_last_game(kube_config_file, namespace_and_container_selection)} points!")
 
 def start_new_game():
     p1 = subprocess.run(get_run_in_refbox_command("rcll-refbox-instruct -c GRIPS"), capture_output=True, text=True)
-    print(p1.returncode)
+    log.info(p1.returncode)
     p2 = subprocess.run(get_run_in_refbox_command("rcll-refbox-instruct -s RUNNING"), capture_output=True, text=True)
-    print(p2.returncode)
+    log.info(p2.returncode)
     p3 = subprocess.run(get_run_in_refbox_command("sleep 15"), capture_output=True, text=True)
-    print(p3.returncode)
+    log.info(p3.returncode)
     p4 = subprocess.run(get_run_in_refbox_command("rcll-refbox-instruct -p PRODUCTION"), capture_output=True, text=True)
-    print(p4.returncode)
+    log.info(p4.returncode)
 
 if __name__ == '__main__':
     app.run(debug=True)
